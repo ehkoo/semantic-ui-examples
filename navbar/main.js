4cc110b049bb6776624e390468ec36a02fbe3d05
kcjpop <kcj.pop.com@gmail.com>
2014-04-13 12:37:01 +0300

    Fix submenu flashing.

--- navbar/main.js
+++ navbar/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-	$('.reorder.icon').on("click",function(){
+	$('.reorder.icon').on("click",function(e){
+        e.preventDefault();
 		$('.ui.vertical.menu').toggle();
 	});
+    
 	$('.ui.dropdown').dropdown();
 });
